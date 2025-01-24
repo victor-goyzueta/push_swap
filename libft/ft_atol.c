810c2f74af027a54a0ac5d5207f1834150676cc0
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-01-25 00:45:18 +0100

    update

---
+++ libft/ft_atol.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 20:00:52 by vgoyzuet          #+#    #+#             */
+/*   Updated: 2025/01/24 20:00:55 by vgoyzuet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atol(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	if ((sign * result) > INT_MAX || (sign * result) < INT_MIN)
+		ft_perror("Overflow detected");
+	return (sign * result);
+}
