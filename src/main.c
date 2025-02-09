9e2e34e51f1d6be1804608fa7c3dc1674ad2784a
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-02-09 17:37:42 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 00:59:35 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2025/02/09 17:19:34 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2025/02/09 17:37:20 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,7 +112,6 @@ int	main(int argc, char **argv)
 	check_digit(&elements);
 	check_duplicated(&elements);
 	init_stack(&a, &elements, &info);
-	ft_print_stack(a, "Stack A: \n");
 	push_swap(&a, &b, info);
 	ft_exit_free(EXIT_FAILURE, &a, &info, NULL);
 }
